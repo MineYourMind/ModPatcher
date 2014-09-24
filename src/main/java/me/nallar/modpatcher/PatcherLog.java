10311b2db7e0132a44d2860b2d7a448c17cba75b
Slind14 <slind014@gmail.com>
2014-09-24 11:33:24 +0200

    added tt logger back in

--- src/main/java/me/nallar/modpatcher/PatcherLog.java
+++ src/main/java/me/nallar/modpatcher/PatcherLog.java
@@ -1,7 +1,13 @@
 package me.nallar.modpatcher;
 
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import cpw.mods.fml.relauncher.FMLRelaunchLog;
+import me.nallar.modpatcher.log.ColorLogFormatter;
+import me.nallar.modpatcher.log.LogFormatter;
+
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.Field;
+import java.util.logging.*;
 
 @SuppressWarnings("UnusedDeclaration")
 public class PatcherLog {
@@ -9,6 +15,99 @@ public class PatcherLog {
 	 * This class might get classloaded twice under different classloaders. Don't do anything important in a static {} block.
 	 */
 	public static final Logger LOGGER = Logger.getLogger("ModPatcher");
+
+    private static Handler handler;
+    private static final int numberOfLogFiles = 20;
+    private static final File logFolder = new File("ModPatcherLogs");
+
+    public static void coloriseLogger() {
+        try {
+            Field consoleLogThreadField = FMLRelaunchLog.class.getDeclaredField("consoleLogThread");
+            consoleLogThreadField.setAccessible(true);
+            Thread consoleLogThreadThread = (Thread) consoleLogThreadField.get(null);
+            Field target = Thread.class.getDeclaredField("target");
+            target.setAccessible(true);
+            Object consoleLogThread = target.get(consoleLogThreadThread);
+            Field handlerField = consoleLogThread.getClass().getDeclaredField("wrappedHandler");
+            handlerField.setAccessible(true);
+            Handler wrappedHandler = (Handler) handlerField.get(consoleLogThreadThread);
+            wrappedHandler.setFormatter(new ColorLogFormatter());
+        } catch (Exception e) {
+            System.err.println("Failed to replace FML logger with colorised logger");
+            e.printStackTrace(System.err);
+        }
+    }
+
+    static {
+        if (!System.getProperty("PatchLogInited", "false").equals("false")) {
+            System.err.println("Initialising extra PatchLog class under classloader " + PatcherLog.class.getClassLoader());
+            System.err.println("Original was under " + System.getProperty("PatchLogInited"));
+            new Throwable().printStackTrace();
+            System.exit(1);
+        }
+        System.setProperty("PatchLogInited", PatcherLog.class.getClassLoader().toString());
+        coloriseLogger();
+        try {
+            final Logger parent = Logger.getLogger("ForgeModLoader");
+            if (parent == null) {
+                throw new NoClassDefFoundError();
+            }
+            LOGGER.setParent(parent);
+            LOGGER.setUseParentHandlers(true);
+        } catch (NoClassDefFoundError ignored) {
+            System.err.println("Failed to get parent logger.");
+            LOGGER.setUseParentHandlers(false);
+            LOGGER.addHandler(new Handler() {
+                private final LogFormatter logFormatter = new LogFormatter();
+
+                @Override
+                public void publish(LogRecord record) {
+                    System.out.print(logFormatter.format(record));
+                }
+
+                @Override
+                public void flush() {
+                }
+
+                @Override
+                public void close() throws SecurityException {
+                }
+            });
+        }
+        setFileName("patcher", LOGGER);
+        LOGGER.setLevel(Level.ALL);
+    }
+
+    public static void setFileName(String name, Logger... loggers) {
+        logFolder.mkdir();
+        for (int i = numberOfLogFiles; i >= 1; i--) {
+            File currentFile = new File(logFolder, name + '.' + i + ".log");
+            if (currentFile.exists()) {
+                if (i == numberOfLogFiles) {
+                    currentFile.delete();
+                } else {
+                    currentFile.renameTo(new File(logFolder, name + '.' + (i + 1) + ".log"));
+                }
+            }
+        }
+        final File saveFile = new File(logFolder, name + ".1.log");
+        saveFile.delete();
+        try {
+            handler = new FileHandler(saveFile.getCanonicalPath());
+            handler.setFormatter(new LogFormatter());
+            for (Logger logger : loggers) {
+                logger.addHandler(handler);
+            }
+        } catch (IOException e) {
+            PatcherLog.error("Can't write logs to disk", e);
+        }
+    }
+
+    public static void flush() {
+        if (handler != null) {
+            handler.flush();
+        }
+    }
 
 	public static void error(String msg) {
 		LOGGER.severe(msg);
